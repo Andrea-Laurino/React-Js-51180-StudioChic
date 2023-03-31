6eddd664fc90a28ff5bf54f0a4ce48b8c1451f2b
ANDREA LAURINO <andreajlaurino@gmail.com>
2023-03-31 19:41:54 -0300

    arreglo en rutas

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,9 @@ import Home from "./components/Home/home";
 import ItemListContainer from './components/ItemListContainer/itemListContainer.jsx'
 import Navbar from './components/Navbar/navbar.jsx'
 import TarjetDescription from "./components/TarjetDescription/tarjetDescription";
-
+import Cart from "./components/Cart/cart"
+import Contact from "./components/Contact/contact"
+import Login from "./components/Loguin/login"
 
 function App() {
   const [productos, setProductos] = useState([]);
@@ -27,15 +29,19 @@ function App() {
         <Route path="/home" element={<Home/>} /> 
         <Route path="/products" element={<ItemListContainer productos={productos}/>} />
         <Route path="/products/:id" element= {<TarjetDescription productos={productos}/>} />
-        <Route path="/products/category/electronics" element={<ItemListContainer productos={productos} category="electronics"/>}/>
+        <Route path="/products/category/electronics" element={<ItemListContainer productos={productos} />}/>
         <Route path="/products/category/electronics/:id"  element= {<TarjetDescription productos={productos}/>} />
-        <Route path="/products/category/jewelery" element={<ItemListContainer productos={productos} category="electronics"/>}/>
+        <Route path="/products/category/jewelery" element={<ItemListContainer productos={productos} />}/>
         <Route path="/products/category/jewelery/:id"  element= {<TarjetDescription productos={productos}/>} />
-        <Route path="/products/category/men's clothing" element={<ItemListContainer productos={productos} category="electronics"/>}/>
+        <Route path="/products/category/men's clothing" element={<ItemListContainer productos={productos} />}/>
         <Route path="/products/category/men's clothing/:id"  element= {<TarjetDescription productos={productos}/>} />
-        <Route path="/products/category/women's clothing" element={<ItemListContainer productos={productos} category="electronics"/>}/>
+        <Route path="/products/category/women's clothing" element={<ItemListContainer productos={productos} />}/>
         <Route path="/products/category/women's clothing/:id"  element= {<TarjetDescription productos={productos}/>} />
         <Route path="/404" element={<img className="img-error" src="src/assets/404.error.jpg" />}/>
+        <Route path="/cart" element={<Cart />}/>
+        <Route path="/contact" element={<Contact />}/>
+        <Route path="/login" element={<Login />}/>
+
     </Routes> 
 
 
